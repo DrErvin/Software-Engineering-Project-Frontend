d6744348c81b14b0ff14d7d77d0c7b1d2689be2a
MrR1ad <riad.elezovic@outlook.com>
2025-05-12 21:03:15 +0200

    Added config.js file
    API and basic local host info

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'http://localhost:3000';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
+export const EMPLOYEE_INFO =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis elit ullamcorper, tempor mauris sed, ultrices enim. Nulla et lacus iaculis, fringilla massa id, lacinia nunc. Fusce nisi nulla, convallis faucibus eleifend varius, dapibus quis tortor.';
