1751f68e845243f8811837ddf721a2cc19b7999a
MrR1ad <riad.elezovic@outlook.com>
2025-05-14 19:47:16 +0200

    Added helpers function for form data

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -27,3 +27,20 @@ export const AJAX = async function (url, uploadData = undefined) {
     throw err;
   }
 };
+
+export const sendFormData = async function (url, formData) {
+  try {
+    const fetchPro = fetch(url, {
+      method: 'POST',
+      body: formData,
+    });
+
+    const res = await fetchPro;
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
