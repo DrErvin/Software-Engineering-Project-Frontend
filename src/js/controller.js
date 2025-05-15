6f3fcf5d4940d9e35e3a5300f9c27ce62cba4f13
MrR1ad <riad.elezovic@outlook.com>
2025-05-15 23:21:03 +0200

    Imported js files till now

---
+++ src/js/controller.js
@@ -0,0 +1,6 @@
+import { RES_PER_PAGE } from './config.js';
+import { MODAL_CLOSE_SEC } from './config.js';
+import * as model from './model.js';
+import featuredView from './views/featuredView.js';
+import SearchView from './views/SearchView.js';
+import loginView from './views/loginView.js';
