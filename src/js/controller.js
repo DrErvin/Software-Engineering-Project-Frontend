acc588fb0bea78781fa0cdb3467763660de50546
MrR1ad <riad.elezovic@outlook.com>
2025-05-17 19:48:47 +0200

    For the featured opportunities on the index page, to render them

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,3 +4,19 @@ import * as model from './model.js';
 import featuredView from './views/featuredView.js';
 import SearchView from './views/SearchView.js';
 import loginView from './views/loginView.js';
+
+const controlFeaturedOpportunities = async function () {
+  try {
+    featuredView.renderSpinner();
+
+    // Fetch data from the server
+    const data = await model.fetchFeatured();
+    console.log('Fetched Featured Opportunities:', data); // Debug log
+
+    // Render the opportunities
+    featuredView.render(data);
+  } catch (err) {
+    featuredView.renderError();
+    console.error(err);
+  }
+};
