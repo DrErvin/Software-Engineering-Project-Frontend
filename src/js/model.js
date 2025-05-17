13415474b4a4a48d71f3055186bf6fc93a128e85
MrR1ad <riad.elezovic@outlook.com>
2025-05-17 21:03:07 +0200

    Loads opportunity
    Finds job with ID, if not then error exception

--- src/js/model.js
+++ src/js/model.js
@@ -51,3 +51,33 @@ const createUserObject = function (account) {
     // companyLocation: user.company_location || '',
   };
 };
+
+export const loadOpportunity = async function (id) {
+  try {
+    // const data = await AJAX(`${API_URL}${id}?key=${KEY}`);
+    const data = await AJAX(`${API_URL}/opportunities`);
+    console.log(data);
+    // console.log(typeof id);
+
+    // Find the opportunity with the specified ID
+    const result = data.find((opportunity) => +opportunity.id === +id);
+    // console.log(result);
+    if (!result) throw new Error(`Opportunity with ID ${id} not found`);
+
+    state.opportunity = createOpportunityObject([result]);
+
+    // Check if recipe that you click on has the same ID as the recipes
+    // stored in the bookmark array in state
+    // This way each recipe you open will always get a bookmakred
+    // tag either true or false
+    // if (state.bookmarks.some((bookmark) => bookmark.id === id))
+    //   state.recipe.bookmarked = true;
+    // else state.recipe.bookmarked = false;
+
+    console.log(state.opportunity);
+  } catch (err) {
+    // Temp error handling
+    console.error(`${err} 💥`);
+    throw err;
+  }
+};
