a97e2562b5a8f044bc6fd2a63f9218f089c71566
MrR1ad <riad.elezovic@outlook.com>
2025-05-19 21:54:18 +0200

    Added the results view code that I had
    Should need some previewVIew I think, to be done sutra. This enhanced the model.js

---
+++ src/js/views/resultsView.js
@@ -0,0 +1,25 @@
+import View from './View.js';
+
+class resultsView extends View {
+  _parentElement = document.querySelector('.container-opp-list');
+  _errorMessage = 'No opportunities found for your query! Please try again';
+  _message = '';
+
+  _sectionsToShow = [
+    document.querySelector('.opportunities-list'),
+    document.querySelector('.intro-section'),
+  ];
+
+  // toggleInit() {
+  //   // this.#toggleSections();
+  //   this.toggleSections(this.#sectionsToShow);
+  // }
+
+  _generateMarkup() {
+    return this._data
+      .map((opportunity) => previewView.render(opportunity, false))
+      .join('');
+  }
+}
+
+export default new resultsView();
